39ec0a607855bcade0928338c5055146ef3ad2bc
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 11:07:39 -0300

    feat: Adicionado Exercício 5 - Salários Mínimos

--- exercicios/Exercicio05SalariosMinimos/SalariosMinimos.java
+++ exercicios/Exercicio05SalariosMinimos/SalariosMinimos.java
@@ -1,4 +1,19 @@
 package Exercicio05SalariosMinimos;
 
+import java.util.Scanner;
+
 public class SalariosMinimos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salarioMinimo= 1293.20;
+
+        System.out.print("Digite o valor do seu salario:");
+        double salarioUsuario= sc.nextDouble();
+
+        double media= salarioUsuario/salarioMinimo;
+
+        System.out.printf("Você recebe %.2f salario(s) mínimo(s)", media);
+
+        sc.close();
+    }
 }
