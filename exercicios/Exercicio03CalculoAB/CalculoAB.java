3a9b414e17909aa4b2d2c5ba33abb0b9b71637aa
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 10:36:56 -0300

    feat: Adicionado Exercício  3 - Calculo A e B

--- exercicios/Exercicio03CalculoAB/CalculoAB.java
+++ exercicios/Exercicio03CalculoAB/CalculoAB.java
@@ -1,4 +1,18 @@
 package Exercicio03CalculoAB;
 
+import java.util.Scanner;
+
 public class CalculoAB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um valor para A: ");
+        int A = sc.nextInt();
+        System.out.print("Digite um valor para B: ");
+        int B = sc.nextInt();
+        if (A == B){
+            int soma = A+B;
+            System.out.println("A soma dos valores é: "+ soma);
+        }
+        sc.close();
+    }
 }
