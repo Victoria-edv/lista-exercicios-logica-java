9bd9b165187c7617515feb0c54b06ff7a10d7e16
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 10:53:25 -0300

    feat: Adicionado Exercício 4 - Antecessor e Sucessor

--- exercicios/Exercicio04AntecessorSucessor/AntecessorSucessor.java
+++ exercicios/Exercicio04AntecessorSucessor/AntecessorSucessor.java
@@ -1,4 +1,18 @@
 package Exercicio04AntecessorSucessor;
 
+import java.util.Scanner;
+
 public class AntecessorSucessor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        int antecessor = numero-1;
+        int sucessor = numero+1;
+
+        System.out.print("Seu antecessor: " + antecessor + " e o seu sucessor: "+ sucessor);
+
+        sc.close();
+    }
 }
