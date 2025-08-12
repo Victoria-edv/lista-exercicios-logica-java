8fcffb891d00a989967f0eef9e759dfc7f28085d
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 15:09:50 -0300

    feat: Adicionado Exercício 6 - Reajuste de 5%

--- exercicios/Exercicio06Reajuste5Porcento/Reajuste5Porcento.java
+++ exercicios/Exercicio06Reajuste5Porcento/Reajuste5Porcento.java
@@ -1,4 +1,16 @@
 package Exercicio06Reajuste5Porcento;
 
+import java.util.Scanner;
+
 public class Reajuste5Porcento {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        double valor = sc.nextDouble();
+        double reajuste = valor + valor*0.05;
+
+        System.out.println("Seu valor com reajuste de 5% é: "+ reajuste);
+
+        sc.close();
+    }
 }
