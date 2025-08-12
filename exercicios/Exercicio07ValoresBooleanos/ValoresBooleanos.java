c8ce383e9834ce06562987b3a2336b3c144451b4
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 16:27:50 -0300

    feat: Adicionado Exercício 7 - Valores Booleanos

--- exercicios/Exercicio07ValoresBooleanos/ValoresBooleanos.java
+++ exercicios/Exercicio07ValoresBooleanos/ValoresBooleanos.java
@@ -1,4 +1,22 @@
 package Exercicio07ValoresBooleanos;
 
+import java.util.Scanner;
+
 public class ValoresBooleanos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um valor booleano (true ou false): ");
+        boolean valor = sc.nextBoolean();
+        System.out.print("Digite novamente um valor booleano (true ou false): ");
+        boolean valor2 = sc.nextBoolean();
+        if (valor && valor2) {
+            System.out.println("Ambos são true, ou seja, é verdadeiro");
+        }
+        else if (!valor && !valor2) {
+            System.out.println("Ambos são falso , ou seja, é false");
+        } else {
+            System.out.println("Os valores são diferentes (um é verdadeiro e o outro é falso).");
+        }
+        sc.close();
+    }
 }
