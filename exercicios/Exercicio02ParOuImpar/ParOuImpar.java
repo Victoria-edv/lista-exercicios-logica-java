e61b5cf77fa64285fac061db1b412f1e6a39b354
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-12 10:18:27 -0300

    feat: Adicionado Exercício 2 - Par ou Ímpar e atualizando

--- exercicios/Exercicio02ParOuImpar/ParOuImpar.java
+++ exercicios/Exercicio02ParOuImpar/ParOuImpar.java
@@ -10,7 +10,7 @@ public class ParOuImpar {
         if (numero % 2 == 0) {
             System.out.println("Esse número é par");
         } else {
-            System.out.println("Esse número é ímpar.");
+            System.out.println("Esse número é ímpar");
         }
         if (numero == 0) {
             System.out.println("O número é zero.");
