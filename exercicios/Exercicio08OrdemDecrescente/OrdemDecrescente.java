d95cb80fb9141bd64c73ec64e7e4d15e855850cc
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-13 10:26:20 -0300

    feat: Adicionado Exercício 8 - Ordem Decrescente

--- exercicios/Exercicio08OrdemDecrescente/OrdemDecrescente.java
+++ exercicios/Exercicio08OrdemDecrescente/OrdemDecrescente.java
@@ -1,4 +1,45 @@
 package Exercicio08OrdemDecrescente;
 
+import java.util.Scanner;
+
 public class OrdemDecrescente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if (A>B && A>C) {
+            System.out.println(A);
+            if (B > C){
+                System.out.println(B);
+                System.out.println(C);
+            } else {
+                System.out.println(C);
+                System.out.println(B);
+            }
+
+        } else if (B>A && B>C) {
+            System.out.println(B);
+            if (C > A){
+                System.out.println(C);
+                System.out.println(A);
+            } else {
+                System.out.println(A);
+                System.out.println(C);
+
+            }
+        } else {
+            System.out.println(C);
+            if (B > A){
+                System.out.println(B);
+                System.out.println(A);
+
+            } else {
+                System.out.println(A);
+                System.out.println(B);
+            }
+        }
+
+    }
 }
