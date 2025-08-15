05b132ca5462edfb8600445c85e48efd070ed422
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-15 14:37:58 -0300

    feat: Adicionado Exercício 12 - Maior ou Menor de Idade

--- exercicios/Exercicio13Maioridade/Maioridade.java
+++ exercicios/Exercicio13Maioridade/Maioridade.java
@@ -1,4 +1,25 @@
 package Exercicio13Maioridade;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Maioridade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.print("Seu nome é "+ nome);
+        if (idade<18){
+            System.out.println(" e você é menor de idade!");
+        } else {
+            System.out.println(" e você é maior de idade!");
+        }
+
+
+
+        sc.close();
+    }
 }
