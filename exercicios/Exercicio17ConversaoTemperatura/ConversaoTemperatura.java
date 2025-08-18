d4bd6a1f0022ccc639919b49228c170a26d8d03f
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 19:00:49 -0300

    feat: Adicionado Exercício 17 - Conversão de Temperatura e atualizado

--- exercicios/Exercicio17ConversaoTemperatura/ConversaoTemperatura.java
+++ exercicios/Exercicio17ConversaoTemperatura/ConversaoTemperatura.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class ConversaoTemperatura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
         System.out.println("Digite uma temperatura em Fahrenheit: ");
         double fahrenheit = sc.nextDouble();
 
         double celsius = (fahrenheit - 32) * 5 / 9;
 
-        System.out.printf(Locale.US,"Em Fahrenheit é: %.2fºF e em Celsius: %.2fºC", fahrenheit, celsius);
+        System.out.printf("Em Fahrenheit é: %.2fºF e em Celsius: %.2fºC", fahrenheit, celsius);
 
         sc.close();
     }
