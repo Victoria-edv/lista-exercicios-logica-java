f625d2f1332f9aabf2d5d2e7c25b43971fea246b
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 19:29:24 -0300

    feat: Adicionado Exercício 19 - Tabuada Completa e atualizado

--- exercicios/Exercicio19TabuadaCompleta/TabuadaCompleta.java
+++ exercicios/Exercicio19TabuadaCompleta/TabuadaCompleta.java
@@ -7,7 +7,7 @@ public class TabuadaCompleta {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        System.out.println("Imprimindo Calculadora de 1 até 10");
+        System.out.println("Imprimindo a calculadora de 1 até 10");
         for (int i =1; i <=10; i++){
             System.out.println("Tabuada do " + i + ":");
             for (int j=1; j<=10; j++){
