2e1bb9f5fc775fb7c101565201b225969e055bb7
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 20:09:01 -0300

    feat: Adicionado Exercício 20 - Tabuada de Um Número

--- exercicios/Exercicio20TabuadaDeUmNumero/TabuadaDeUmNumero.java
+++ exercicios/Exercicio20TabuadaDeUmNumero/TabuadaDeUmNumero.java
@@ -1,4 +1,19 @@
 package Exercicio20TabuadaDeUmNumero;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TabuadaDeUmNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        System.out.println("Digite um valor: ");
+        int valor = sc.nextInt();
+        System.out.print("Tabuada do " + valor + ": ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(valor + " x " + i + " = " + (valor * i));
+        }
+        sc.close();
+
+    }
 }
