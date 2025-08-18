93309b0f7ad836ae93ad0c0a211ce59087bbfc7f
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 14:50:54 -0300

    docs: Adicionado comentário sobre a suposição de data no Exercício 15

--- exercicios/Exercicio15TempoDeVida/TempoDeVida.java
+++ exercicios/Exercicio15TempoDeVida/TempoDeVida.java
@@ -10,10 +10,12 @@ public class TempoDeVida {
         System.out.print("Em que ano você nasceu? ");
         int anoNasc = sc.nextInt();
 
+        // Dados fixos de referência para o cálculo
         int anoAtual = 2025;
         int mesAtual = 8;
         int diaAtual = 18;
 
+        // O cálculo assume que a pessoa nasceu em 01/01 do seu ano de nascimento.
         int anosCompletos = anoAtual - anoNasc;
         int diasPassadosAnoAtual = (mesAtual - 1) * 30 + diaAtual;
         int totalDias = (anosCompletos * 365) + diasPassadosAnoAtual;
