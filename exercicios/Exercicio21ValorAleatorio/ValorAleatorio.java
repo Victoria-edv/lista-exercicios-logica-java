2ae818a5bdc7689d8733055ad74fd7ee9f18a621
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 20:17:48 -0300

    feat: Adicionado Exercício 21 - Valor Aleatório

--- exercicios/Exercicio21ValorAleatorio/ValorAleatorio.java
+++ exercicios/Exercicio21ValorAleatorio/ValorAleatorio.java
@@ -1,4 +1,21 @@
 package Exercicio21ValorAleatorio;
 
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ValorAleatorio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Selecionando um valor aleatório");
+
+        Random gerador = new Random();
+        int valorAleatorio = gerador.nextInt(101);
+
+        System.out.println("O valor aleatório entre 0 e 100 foi... " + valorAleatorio + " !!!!");
+
+        sc.close();
+    }
 }
