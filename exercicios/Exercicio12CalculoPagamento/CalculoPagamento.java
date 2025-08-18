cb1629c8361107c52009fd00cc4cdd7feb408030
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 18:54:48 -0300

    feat: Adicionado Exercício 12 - Cálculo para Pagamento e atualizado

--- exercicios/Exercicio12CalculoPagamento/CalculoPagamento.java
+++ exercicios/Exercicio12CalculoPagamento/CalculoPagamento.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class CalculoPagamento {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
         System.out.println("Qual o valor do seu produto? ");
         double valor = sc.nextDouble();
         System.out.println("Qual a forma de pagamento?");
-        System.out.println("1 - À Vista em Dinheiro ou Pix, recebe 15% de desconto");
-        System.out.println("2 - À Vista no cartão de crédito, recebe 10% de desconto");
-        System.out.println("3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros");
-        System.out.println("4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais juros de 10%");
+        System.out.println(" 1 - À Vista em Dinheiro ou Pix, recebe 15% de desconto");
+        System.out.println(" 2 - À Vista no cartão de crédito, recebe 10% de desconto");
+        System.out.println(" 3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros");
+        System.out.println(" 4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais juros de 10%");
         double valorFinal = 0;
         boolean codigoInvalido = false;
         int codigoPagamento = sc.nextInt();
@@ -31,7 +31,7 @@ public class CalculoPagamento {
                 valorFinal = valor + valor * 0.10;
                 break;
             default:
-                System.out.println("Opção de pagamento inválida.");
+                System.out.println("Opção de pagamento inválida");
                 codigoInvalido = true;
         }
         if (!codigoInvalido) {
