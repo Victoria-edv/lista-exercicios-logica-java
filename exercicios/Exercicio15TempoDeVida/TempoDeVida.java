edcf7761096b704e0c14bd296db7e8948e5fcde0
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 18:57:06 -0300

    Exercício 15 Atualizado

--- exercicios/Exercicio15TempoDeVida/TempoDeVida.java
+++ exercicios/Exercicio15TempoDeVida/TempoDeVida.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class TempoDeVida {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
         System.out.print("Em que ano você nasceu? ");
         int anoNasc = sc.nextInt();
 
