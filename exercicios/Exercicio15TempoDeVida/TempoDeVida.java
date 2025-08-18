9fdccf1f3b4707d8ca958442cb93119f17f13572
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 11:27:18 -0300

    feat: Adicionado Exercício 15 - Tempo de Vida

--- exercicios/Exercicio15TempoDeVida/TempoDeVida.java
+++ exercicios/Exercicio15TempoDeVida/TempoDeVida.java
@@ -1,4 +1,31 @@
 package Exercicio15TempoDeVida;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TempoDeVida {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.print("Em que ano você nasceu? ");
+        int anoNasc = sc.nextInt();
+
+        int anoAtual = 2025;
+        int mesAtual = 8;
+        int diaAtual = 18;
+
+        int anosCompletos = anoAtual - anoNasc;
+        int diasPassadosAnoAtual = (mesAtual - 1) * 30 + diaAtual;
+        int totalDias = (anosCompletos * 365) + diasPassadosAnoAtual;
+
+        int anos = totalDias / 365;
+        int diasRestantes = totalDias % 365;
+
+        int meses = diasRestantes / 30;
+        int dias = diasRestantes % 30;
+
+        System.out.println("Você tem " + anos + " anos, " + meses + " meses e " + dias + " dias de vida.");
+
+        sc.close();
+    }
 }
