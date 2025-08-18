a40e7da49f551cfafd670d09de4d97e7c8f8c23d
Victoria-edv <victoriacarvalho.edv@gmail.com>
2025-08-18 20:28:55 -0300

    feat: Adicionado Exercício 22 - Valores de Quociente e Resto

--- exercicios/Exercicio22QuocienteResto/QuocienteResto.java
+++ exercicios/Exercicio22QuocienteResto/QuocienteResto.java
@@ -1,4 +1,24 @@
 package Exercicio22QuocienteResto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class QuocienteResto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite um valor para A: ");
+        int A = sc.nextInt();
+        System.out.print("Digite um valor para B: ");
+        int B = sc.nextInt();
+
+        int quociente = A/B;
+        System.out.print("Quociente: "+ quociente );
+
+        int resto = A % B;
+        System.out.print("Resto: " + resto);
+
+        sc.close();
+    }
 }
